55713c21810835f91b51abb271f95013c4588a18
Polar Bear <59422282+rhwjwkdeks@users.noreply.github.com>
2021-03-31 16:14:46 -0500

    update

---
+++ Assets/cubeScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cubeScript : MonoBehaviour
+{
+
+    private GameObject cube;
+    private GameObject coffee;
+    private GameObject cheese;
+    private GameObject piano;
+    private GameObject lunar;
+    private Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GameObject.Find("Cube");
+        coffee = GameObject.Find("CoffeeCup");
+        cheese = GameObject.Find("Cheese");
+        piano = GameObject.Find("Piano");
+        lunar = GameObject.Find("Lunar");
+
+        score = GameObject.Find("score").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "score1")
+        {
+            score.text = "A Coffe Cup!";
+            cube.SetActive(false);
+            cube.transform.position = new Vector3(0, 0.14f, 2.47f);
+            cube.SetActive(true);
+
+            GameObject CC = GameObject.Instantiate(coffee);
+            CC.transform.position = new Vector3(0, 0.5f, 2.47f);
+        }
+        else if (collision.gameObject.name == "score2")
+        {
+            score.text = "A Piece of Cheese!";
+            cube.SetActive(false);
+            cube.transform.position = new Vector3(0, 0.14f, 2.47f);
+            cube.SetActive(true);
+
+            GameObject CC = GameObject.Instantiate(cheese);
+            CC.transform.position = new Vector3(0, 0.5f, 2.47f);
+        }
+        else if (collision.gameObject.name == "score3")
+        {
+            score.text = "A Part Of Piano!";
+            cube.SetActive(false);
+            cube.transform.position = new Vector3(0, 0.14f, 2.47f);
+            cube.SetActive(true);
+
+            GameObject CC = GameObject.Instantiate(piano);
+            CC.transform.position = new Vector3(0, 0.5f, 2.47f);
+        }
+        else if (collision.gameObject.name == "score4")
+        {
+            score.text = "Waite.. It Looks Like A Spacecraft..";
+            cube.SetActive(false);
+            cube.transform.position = new Vector3(0, 0.14f, 2.47f);
+            cube.SetActive(true);
+
+            GameObject CC = GameObject.Instantiate(lunar);
+            CC.transform.position = new Vector3(0, 0.5f, 2.47f);
+        }
+        else if(collision.gameObject.name == "safeGround")
+        {
+            score.text = "You Dropped The Cube, Be Careful!";
+            cube.SetActive(false);
+            cube.transform.position = new Vector3(0, 0.14f, 2.47f);
+            cube.SetActive(true);
+        }
+    }
+}
---
+++ Assets/getScore.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getScore : MonoBehaviour
+{
+
+    private Text score;
+    private GameObject score1;
+    private GameObject score2;
+    private GameObject score3;
+    private GameObject score4;
+    private GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GameObject.Find("score").GetComponent<Text>();
+
+        score1 = GameObject.Find("score1");
+        score2 = GameObject.Find("score2");
+        score3 = GameObject.Find("score3");
+        score4 = GameObject.Find("score4");
+
+        cube = GameObject.Find("Cube");
+
+
+
+        score.text = "Touch White Panels With The Cube For Random Events!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
